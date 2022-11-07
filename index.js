78ccb26d9729b80f3202e3eb2b7eeccecf039b68
harunorrashid4419 <harunrashid4419@gmail.com>
2022-11-07 22:15:03 +0600

    express add

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uqseuad.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 
 app.get('/', (req, res) =>{
     res.send('wifi service is running')
