58e38646c652e2bb076c558c4c8fe326e9dfc042
harunorrashid4419 <harunrashid4419@gmail.com>
2022-11-07 23:46:43 +0600

    service get

--- index.js
+++ index.js
@@ -11,11 +11,24 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uqseuad.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+async function run(){
+    try{
+        const serviceList = client.db('ourService').collection('services');
+
+        app.get('/services', async(req, res) =>{
+            const query = {};
+            const cursor = serviceList.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.error(error))
 
 
 
