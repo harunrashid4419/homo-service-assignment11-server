e4bf0c45f4aa46e987751bdffe4c4101875873dc
harunorrashid4419 <harunrashid4419@gmail.com>
2022-11-08 19:03:11 +0600

    service add compleate

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -17,10 +17,29 @@ async function run(){
     try{
         const serviceList = client.db('ourService').collection('services');
 
+        app.post('/allServices', async(req, res) =>{
+            const query = req.body;
+            const result = await serviceList.insertOne(query);
+            res.send(result);
+        })
         app.get('/services', async(req, res) =>{
             const query = {};
             const cursor = serviceList.find(query);
+            const result = await cursor.limit(3).toArray();
+            res.send(result);
+        });
+
+        app.get('/allServices', async(req, res) =>{
+            const query = {};
+            const cursor = serviceList.find(query);
             const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get('/allServices/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await serviceList.findOne(query);
             res.send(result);
         })
     }
